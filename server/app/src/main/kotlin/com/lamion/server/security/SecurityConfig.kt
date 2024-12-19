c57ce3e92237f1976f482029fb1d29de29c2aa86
ImpossibleAccuracy <rvarzin1@gmail.com>
2024-12-19 23:27:33 +0500

    updated password encoder

--- server/app/src/main/kotlin/com/lamion/server/security/SecurityConfig.kt
+++ server/app/src/main/kotlin/com/lamion/server/security/SecurityConfig.kt
@@ -2,14 +2,20 @@ package com.lamion.server.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 class SecurityConfig {
     @Bean
-    fun passwordEncoder(): PasswordEncoder {
-//        return BCryptPasswordEncoder()
+    fun passwordEncoder(environment: Environment): PasswordEncoder {
+        if (environment.activeProfiles.contains("prod")) {
+            return BCryptPasswordEncoder()
+        }
+
+        @Suppress("DEPRECATION")
         return NoOpPasswordEncoder.getInstance()
     }
 }
